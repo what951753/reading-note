5345c903d6a736585350cc60843f33c22c093d56
what951753 <69564154+what951753@users.noreply.github.com>
2022-08-05 00:03:33 +0800

    Deploy test v2

--- src/main/java/com/example/restservice/Greeter.java
+++ src/main/java/com/example/restservice/Greeter.java
@@ -3,4 +3,9 @@ package com.example.restservice;
 public class Greeter {
     private String name ;
     private int count;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
 }
